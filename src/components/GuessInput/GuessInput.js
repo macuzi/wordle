0f89b18d9c1c1b1968e3a3699715afff971d2727
lamberto <asghedom.lamberto@gmail.com>
2024-08-30 09:49:34 -0700

    logging input value

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function GuessInput() {
+  const [tentativeGuess, setTentativeGuess] = useState("");
+
+  function handleSubmitGuess(event) {
+    event.preventDefault();
+    console.log({ guess: tentativeGuess });
+    setTentativeGuess("");
+  }
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmitGuess}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        minLength={5}
+        maxLength={5}
+        value={tentativeGuess}
+        onChange={(event) => {
+          const nextGuess = event.target.value.toUpperCase();
+          setTentativeGuess(nextGuess);
+        }}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;

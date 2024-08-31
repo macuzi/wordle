b3e6f2c36c66a49403932048b4fffaf726bf8f3b
lamberto <asghedom.lamberto@gmail.com>
2024-08-31 09:09:05 -0700

    Cell Component

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -3,6 +3,15 @@ import { range } from "/src/utils.js";
 
 import { checkGuess } from "../../game-helpers";
 
+/* 
+ > props passed from return statment 
+ > "letter" from value[num] / "status" from result[num].status
+*/
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : "cell";
+  return <span className={className}>{letter}</span>;
+}
+
 function Guess({ value, answer }) {
   const result = checkGuess(value, answer);
 
@@ -11,9 +20,11 @@ function Guess({ value, answer }) {
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
-          {value ? value[num] : undefined}
-        </span>
+        <Cell
+          key={num}
+          letter={result ? result[num].letter : undefined}
+          status={result ? result[num].status : undefined}
+        />
       ))}
     </p>
   );
